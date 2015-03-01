1b0d4a0c47aed1a55e6c20e8a782953d9c19831f
mmetelskiy <met.ms@mail.ru>
2015-03-02 00:01:54 +0300

    just commit

---
+++ script/variables.js
@@ -0,0 +1,6 @@
+var textarea = document.getElementById("textarea");
+var messageContainer = document.getElementById("message-container");
+var modalSkeletone = document.getElementById("modal-container");
+var form = document.getElementById("form");
+var sendOnEnter = false;
+var modalIsShown = false;
